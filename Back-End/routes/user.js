30912e410fcb734e30063e87ab00bd26974b5362
chama97 <chamamanchanayake5@gmail.com>
2022-08-25 17:46:05 +0530

    user complete

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -35,4 +35,45 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.get('/:id', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    }catch(err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const response = await user.remove()
+
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        user.name = {
+            firstname: req.body.name.firstname,
+            lastname: req.body.name.lastname,
+        },
+        user.gender = req.body.gender,
+        user.dateOfBirth = req.body.dateOfBirth,
+        user.email = req.body.email,
+        user.password = req.body.password,
+        user.contact = req.body.contact
+
+        const response = await user.save()
+        res.json(response)
+
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
 module.exports = router
